80c0f59144eb296c6a8182d4b74cba757f919f96
Jonas Gottschau <jonas@gottschau.dk>
2017-04-24 22:06:16 +0200

    100 jobs, longruning (10s) big args (30 properties)

--- src/Hangfire.Mongo.Sample.NETCore/Program.cs
+++ src/Hangfire.Mongo.Sample.NETCore/Program.cs
@@ -1,21 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Hangfire.Mongo.Sample.NETCore
 {
     public class Program
     {
-        private const int JobCount = 10;
+        private const int JobCount = 100;
 
+        public class BigKahuna : Dictionary<string, string>
+        {
+            public static BigKahuna Create(int propertyCount)
+            {
+                var obj = new BigKahuna();
+                for (int i = 0; i < propertyCount; i++)
+                {
+                    obj[$"Property{i}"] = $"PropertyVaule{i}";
+                }
+                return obj;
+            }
+        }
+
+        public static DateTime StarTime;
+        public static ManualResetEvent Signal = new ManualResetEvent(false);
         public static void Main(string[] args)
         {
             JobStorage.Current = new MongoStorage("mongodb://localhost", "Mongo-Hangfire-Sample-NETCore");
 
-            using (new BackgroundJobServer())
+            using (new BackgroundJobServer(new BackgroundJobServerOptions
             {
+                WorkerCount = 25
+            }))
+            {
+                Console.WriteLine("Running a single dummy job to create all db colletions");
+                BackgroundJob.Enqueue(() => Warmup());
+
+                Signal.WaitOne();
+
                 for (var i = 0; i < JobCount; i++)
                 {
-                    var jobId = i;
-                    BackgroundJob.Enqueue(() => Console.WriteLine($"Fire-and-forget ({jobId})"));
+                    var last = i == JobCount - 1;
+                    var jobCount = i;
+                    var bigkahuna = BigKahuna.Create(30);
+                    BackgroundJob.Enqueue(() => Job(bigkahuna, jobCount, last));
                 }
 
                 Console.WriteLine($"{JobCount} job(s) has been enqued. They will be executed shortly!");
@@ -26,6 +53,24 @@ namespace Hangfire.Mongo.Sample.NETCore
                 Console.WriteLine($"Press any key to exit...");
 
                 Console.ReadKey(true);
+            }
+        }
+
+        public static void Warmup()
+        {
+            Signal.Set();
+        }
+        public static void Job(BigKahuna bigKahuna, int jobNumber, bool last)
+        {
+            if (jobNumber == 0)
+            {
+                StarTime = DateTime.Now;
+            }
+            Thread.Sleep(10000);
+            if (last)
+            {
+                var elapsedMs = (DateTime.Now - StarTime).TotalMilliseconds;
+                Console.WriteLine($"Processed {jobNumber + 1} longrunning jobs in {elapsedMs} ms");
             }
         }
     }
